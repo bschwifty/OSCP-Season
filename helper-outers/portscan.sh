2a863580e98903a4228df404debde44c7b7a90a7
bschwifty <99057740+bschwifty@users.noreply.github.com>
2024-12-05 12:43:13 -0700

    Update portscan.sh
    updated to take input

--- helper-outers/portscan.sh
+++ helper-outers/portscan.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # a very simple port scanner
-host=10.5.5.11   # change this ip
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <IP_ADDRESS>"
+    exit 1
+fi
+
+host=$1
+
 for port in {1..65535}; do
     timeout .1 bash -c "echo >/dev/tcp/$host/$port" &&
         echo "port $port is open"
